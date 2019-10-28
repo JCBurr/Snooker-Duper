7d2a91341333c9c50c2d035fb3991b47c6b1826a
James Burr <jamescburr94@gmail.com>
2019-10-28 20:33:07 +0000

    Add colliders
    Added mesh collider to snooker table and balls.
    Mesh requires more editing as balls currently don't land on the rails underneath pockets and the pockets are unreasonably small.'
    Added gitignore file.

---
+++ Assets/Scripts/CueBallScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueBallScript : MonoBehaviour {
+
+    Rigidbody body;
+    public Camera mainCamera;
+    GameObject gameManager;
+    Component manageTurnScript;
+
+    float shotPower = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        gameManager = GameObject.FindWithTag("GameController");
+		
+	}
+
+    // Physics function to apply force to the ball
+    private void HitBall()
+    {
+            body.AddForce((new Vector3(mainCamera.transform.forward.x,
+                0.0f, mainCamera.transform.forward.z) * shotPower), ForceMode.Impulse);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonUp(0) && gameManager.GetComponent<manage_turn>().turnIsActive == false)
+        {
+            HitBall();
+        }
+	}
+}
---
+++ Assets/Scripts/manage_turn.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manage_turn : MonoBehaviour {
+
+    public bool turnIsActive = false;
+    float velocity = 0.0f;
+
+    public GameObject cueBall;
+
+    public Cinemachine.CinemachineFreeLook aimingCamera;
+    public Cinemachine.CinemachineVirtualCamera spiderCam;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        velocity = cueBall.GetComponent<Rigidbody>().velocity.magnitude;
+        if (velocity > 0 && turnIsActive == false)
+        {
+            ActivateTurn();
+        }
+
+        if (velocity < 0.005 && turnIsActive == true)
+        {
+            DeactivateTurn();
+        }
+	}
+
+    // Called when the cue ball begins moving (turn is started)
+    void ActivateTurn()
+    {
+        turnIsActive = true;
+        SwapCameraPriority();
+    }
+
+
+    // Called when the cue ball stops moving (turn ends, return to aiming)
+    void DeactivateTurn()
+    {
+        turnIsActive = false;
+        SwapCameraPriority();
+    }
+
+
+    // Called when turn starts and ends. Swaps priority between aim camera and shot
+    // camera.
+    void SwapCameraPriority()
+    {
+        if (aimingCamera.Priority > spiderCam.Priority)
+        {
+            aimingCamera.Priority--;
+            spiderCam.Priority++;
+        }
+        else
+        {
+            spiderCam.Priority--;
+            aimingCamera.Priority++;
+        }
+    }
+}
